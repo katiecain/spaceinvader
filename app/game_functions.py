86151bb8d6896f1f630266fe0b63f75c57916df2
katiecain <kaitlynjcaindev@gmail.com>
2017-06-24 13:30:03 -0400

    refactoring event handling to break up the functions

--- app/game_functions.py
+++ app/game_functions.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 
+# Event handling
 def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
@@ -10,16 +11,24 @@ def check_events(ship):
             sys.exit()
         # Ship movement
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+# Screen handling
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
     screen.fill(ai_settings.bg_color)
