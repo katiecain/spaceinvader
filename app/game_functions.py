d2ff8be72db1e5fa1547c4ef7b8bdbe4f0ab4255
katiecain <kaitlynjcaindev@gmail.com>
2017-06-24 17:07:53 -0400

    finishing play button

--- app/game_functions.py
+++ app/game_functions.py
@@ -8,14 +8,15 @@ from app.alien import Alien
 
 
 # Event handling
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            if not stats.game_active:
+                check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
         # Ship movement
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, ai_settings, stats, screen, ship, bullets)
@@ -23,10 +24,21 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks play"""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+
+    if button_clicked and not stats.game_active:
+        pygame.mouse.set_visible(False)
+        stats.reset_stats()
         stats.game_active = True
+        aliens.empty()
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+    else:
+        stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_keydown_events(event, ai_settings, stats, screen, ship, bullets):
