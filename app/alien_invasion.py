fdcc8beb104c803f26575e879eea3b501591f8fa
katiecain <kaitlynjcaindev@gmail.com>
2017-06-24 11:05:09 -0400

    creating settings and adding ship image

--- app/alien_invasion.py
+++ app/alien_invasion.py
@@ -2,11 +2,14 @@ import sys
 
 import pygame
 
+from app.settings import Settings
+
 
 def run_game():
     # Initializegame and create a screen object
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     bg_color=(230, 230, 230)
@@ -16,7 +19,7 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            screen.fill(bg_color)
+            screen.fill(ai_settings.bg_color)
 
     # Make the most recent drawn screen visible
         pygame.display.flip()
