a68129a1d332067469f6dfb1c7fb1a62ddb6dd09
katiecain <kaitlynjcaindev@gmail.com>
2017-06-24 11:21:42 -0400

    refactoring the event listener

--- app/alien_invasion.py
+++ app/alien_invasion.py
@@ -1,9 +1,9 @@
-import sys
-
 import pygame
 
 from app.settings import Settings
 from app.ship import Ship
+import app.game_functions as gf
+
 
 def run_game():
     # Initializegame and create a screen object
@@ -19,13 +19,12 @@ def run_game():
 
     # Start the main loop for the game
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            screen.fill(ai_settings.bg_color)
-            ship.blitme()
+        gf.check_events()
 
-    # Make the most recent drawn screen visible
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
+        # Make the most recent drawn screen visible
         pygame.display.flip()
 
 run_game()
