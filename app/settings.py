aa40ff545e1f04acf5d44c85de0fc744a262be94
katiecain <kaitlynjcaindev@gmail.com>
2017-06-24 17:18:45 -0400

    adding speed increase settings

--- app/settings.py
+++ app/settings.py
@@ -8,11 +8,9 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed_factor = 1.5
         self.ship_limit = 2
 
         # Bullet settings
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
@@ -20,7 +18,23 @@ class Settings:
 
         # Alien settings
         self.alien_space = 1.5
-        self.alien_speed_factor = 0.5
         self.fleet_drop_speed = 10
-        # Fleet_direction of 1 represents right and -1 is left
+
+        # Level up settings
+        self.speedup_scale = 1.1
+
+        self.init_dynamic_settings()
+
+
+    def init_dynamic_settings(self):
+        """Initialize settings that change throughout the game"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 0.5
         self.fleet_direction = 1
+
+
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
