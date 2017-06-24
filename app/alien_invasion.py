a97c02d408e6d0c459f99587cb98b6f7e02f7027
katiecain <kaitlynjcaindev@gmail.com>
2017-06-24 11:14:52 -0400

    creating a ship

--- app/alien_invasion.py
+++ app/alien_invasion.py
@@ -3,7 +3,7 @@ import sys
 import pygame
 
 from app.settings import Settings
-
+from app.ship import Ship
 
 def run_game():
     # Initializegame and create a screen object
@@ -11,6 +11,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+    ship = Ship(screen)
 
     bg_color=(230, 230, 230)
 
@@ -20,6 +23,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
             screen.fill(ai_settings.bg_color)
+            ship.blitme()
 
     # Make the most recent drawn screen visible
         pygame.display.flip()
